9af391de49800050ca8e32e00694892c1499ff6c
williampetitpierre <williampp@hotmail.fr>
2024-10-08 10:13:49 +0200

    refactoring resoc_n2

--- resoc_n2/usurpedpost.php
+++ resoc_n2/usurpedpost.php
@@ -10,24 +10,9 @@ session_start();
         <link rel="stylesheet" href="style.css"/>
     </head>
     <body>
-        <header>
-            <img src="resoc.jpg" alt="Logo de notre réseau social"/>
-            <nav id="menu">
-                <a href="news.php">Actualités</a>
-                <a href="wall.php?user_id=5">Mur</a>
-                <a href="feed.php?user_id=5">Flux</a>
-                <a href="tags.php?tag_id=1">Mots-clés</a>
-            </nav>
-            <nav id="user">
-                <a href="#">Profil</a>
-                <ul>
-                    <li><a href="settings.php?user_id=5">Paramètres</a></li>
-                    <li><a href="followers.php?user_id=5">Mes suiveurs</a></li>
-                    <li><a href="subscriptions.php?user_id=5">Mes abonnements</a></li>
-                </ul>
-
-            </nav>
-        </header>
+    <?php 
+        include '../resoc_n1/header.php';
+        ?>
 
         <div id="wrapper" >
 
@@ -43,13 +28,13 @@ session_start();
                     /**
                      * BD
                      */
-                    $mysqli = new mysqli("localhost", "root", "root", "socialnetwork_tests");
+                    include '../resoc_n1/database-connection.php';
                     /**
                      * Récupération de la liste des auteurs
                      */
                     $listAuteurs = [];
                     $laQuestionEnSql = "SELECT * FROM users";
-                    $lesInformations = $mysqli->query($laQuestionEnSql);
+                    include '../resoc_n1/query-response.php';
                     while ($user = $lesInformations->fetch_assoc())
                     {
                         $listAuteurs[$user['id']] = $user['alias'];
