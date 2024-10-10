48b8c83d11597c1c82c47fe1c69e008ea0c5e29b
Alix Leve <alix.leve@gmail.com>
2024-10-10 17:10:08 +0200

    added news-display.php

---
+++ resoc_n1/news/news-display.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>ReSoC - Actualités</title> 
+        <meta name="author" content="Julien Falconnet">
+        <link rel="stylesheet" href="../style.css"/>
+    </head>
+    <body>
+    <?php 
+        include '../main/header.php';
+    ?>
+        <div id="wrapper">
+            <aside>
+                <img src="user.jpg" alt="Portrait de l'utilisatrice"/>
+                <section>
+                    <h3>Présentation</h3>
+                    <p>Sur cette page vous trouverez les derniers messages de
+                        tous les utilisatrices du site.</p>
+                </section>
+            </aside>
+            <main>
+                <?php
+                include 'news-query.php';
+                $postDataAnswer = getPostData($mysqli);
+                while ($postDataRendering = $postDataAnswer->fetch_assoc())
+                {
+                
+                    ?>
+                    <?php 
+                    
+                        // $newsPost = renderPost();
+                        ?>
+                    <?php
+                }
+                ?>
+
+            </main>
+        </div>
+    </body>
+</html>
