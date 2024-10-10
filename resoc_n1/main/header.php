c449e344095f96d7bb399e029e088d900f9da07e
williampetitpierre <williampp@hotmail.fr>
2024-10-10 16:38:44 +0200

    fixed: feed logic, feed header link; add: queries.php

--- resoc_n1/main/header.php
+++ resoc_n1/main/header.php
@@ -1,9 +1,9 @@
 <header>
-    <img src="resoc.jpg" alt="Logo de notre réseau social" />
+    <img src="../../resoc_n1/resoc.jpg" alt="Logo de notre réseau social" />
     <nav id="menu">
         <a href="../resoc_n1/news.php">Actualités</a>
         <a href="../resoc_n1/wall.php?user_id=5">Mur</a>
-        <a href="../resoc_n1/feed.php?user_id=5">Flux</a>
+        <a href="../../resoc_n1/feed/feed-display.php">Flux</a>
         <a href="../resoc_n1/tags.php?tag_id=1">Mots-clés</a>
     </nav>
     <nav id="user">
