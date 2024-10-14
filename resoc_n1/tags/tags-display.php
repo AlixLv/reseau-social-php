7ccd20914eb82005fc11725ecf972a253fe8ab21
annekaftal <anne.kaftal@gmail.com>
2024-10-14 12:26:54 +0200

    Fixed: début de refactorisation tags.php

--- resoc_n1/tags/tags-display.php
+++ resoc_n1/tags/tags-display.php
@@ -8,6 +8,7 @@
     </head>
     <body>
     <?php 
+        include 'tags-queries.php';
         include '../main/header.php';            
         include '../main/main-utilities.php';
     ?>
---
+++ resoc_n1/tags/tags-queries.php
@@ -0,0 +1,23 @@
+<?php 
+
+function getPostsByTag($tag){
+    return "SELECT posts.content,
+        posts.created,
+        posts.id as post_id,
+        users.alias as author_name,  
+        count(likes.id) as like_number,  
+        users.id as user_id,
+        GROUP_CONCAT(DISTINCT tags.label) AS taglist 
+        FROM posts_tags as filter 
+        JOIN posts ON posts.id=filter.post_id
+        JOIN users ON users.id=posts.user_id
+        LEFT JOIN posts_tags ON posts.id = posts_tags.post_id  
+        LEFT JOIN tags       ON posts_tags.tag_id  = tags.id 
+        LEFT JOIN likes      ON likes.post_id  = posts.id 
+        WHERE filter.tag_id = '$tag' 
+        GROUP BY posts.id
+        ORDER BY posts.created DESC";
+}
+
+
+?>
